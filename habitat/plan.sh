c9ffd172a3f4c5ecbcd107ddb6485959734136c7
Collin McNeese <collin@mydevsandbox.com>
2022-05-22 08:13:05 -0500

    License Update

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=effortless_audit_example
 pkg_version=0.1.1
 pkg_origin=collinmcneese
-pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
+pkg_maintainer="Collin McNeese <collin@mydevsandbox.com>"
 pkg_license=("Apache-2.0")
 pkg_description="Effortless Audit Example for Linux"
 pkg_scaffolding="chef/scaffolding-chef-inspec"
